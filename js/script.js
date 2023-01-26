3661598da0239a0647ef9bfe6661502c79a658e9
Valentyn <riodemouro2017@gmail.com>
2023-01-26 07:29:46 +0200

    swiper

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-$(function () {
-  $(".heading-top__right").slick();
+"use strict";
+const swiper = new Swiper(".heading-top__right", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
