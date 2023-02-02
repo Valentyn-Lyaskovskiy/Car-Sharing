5647c28b9ad6142717d9e487535c3b8414162e94
Valentyn Lyaskovskiy <riodemouro2017@gmail.com>
2023-02-02 18:01:17 +0200

    .

--- js/script.js
+++ js/script.js
@@ -15,10 +15,44 @@ const swiper2 = new Swiper(".mySwiper", {
   slidesPerGroup: 1,
   loop: true,
   loopFillGroupWithBlank: false,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    1026: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    1220: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
   pagination: {
     el: ".swiper-pagination",
   },
 });
+
+// const swiperEl = document.querySelector("swiper-wrapper");
+// Object.assign(swiperEl, {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   pagination: {
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     1027: {
+//       slidesPerView: 4,
+//       spaceBetween: 30,
+//     },
+//     1026: {
+//       slidesPerView: 2,
+//       spaceBetween: 10,
+//     },
+//   },
+// });
+// swiperEl.initialize();
 
 const burger = document.querySelector(".menu__btn");
 const menu = document.querySelector(".menu__list");
