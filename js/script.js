070573cbc9c2f0693c10c375c7fdbc56447ca310
Valentyn <riodemouro2017@gmail.com>
2023-02-03 00:03:53 +0200

    padding

--- js/script.js
+++ js/script.js
@@ -34,26 +34,6 @@ const swiper2 = new Swiper(".mySwiper", {
   },
 });
 
-// const swiperEl = document.querySelector("swiper-wrapper");
-// Object.assign(swiperEl, {
-//   slidesPerView: 1,
-//   spaceBetween: 10,
-//   pagination: {
-//     clickable: true,
-//   },
-//   breakpoints: {
-//     1027: {
-//       slidesPerView: 4,
-//       spaceBetween: 30,
-//     },
-//     1026: {
-//       slidesPerView: 2,
-//       spaceBetween: 10,
-//     },
-//   },
-// });
-// swiperEl.initialize();
-
 const burger = document.querySelector(".menu__btn");
 const menu = document.querySelector(".menu__list");
 burger.addEventListener("click", function () {
